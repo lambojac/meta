938b0270b5e691be505c2951e96904df6bd8f42c
adamokennie <adamokennie@gmail.com>
2024-02-13 19:34:49 +0100

    commit

--- src/components/Slider/ExploreSection.js
+++ src/components/Slider/ExploreSection.js
@@ -1,41 +1,50 @@
-import React from 'react';
-import Slider from 'react-slick';
-import './ExploreSection.css';
+import React from 'react'
+import EastOutlinedIcon from '@mui/icons-material/EastOutlined'
+import WestOutlinedIcon from '@mui/icons-material/WestOutlined'
+import { useState } from 'react'
+import "./ExploreSection.css"
 
-const ExploreSection = () => {
-  const carouselSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  };
+function ExploreSection() {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const data = [
+    "./img1.png",
+    "./img2.png",
+    "./img3.png",
+  ]
 
-  const images = [
-    { src: 'image1.jpg', alt: 'Image 1' },
-    { src: 'image2.jpg', alt: 'Image 2' },
-    { src: 'image3.jpg', alt: 'Image 3' },
-    // Add more images as needed
-  ];
+  const prevSlide = () => {
+    setCurrentSlide((prev) => { return prev === 0 ? 1 : prev - 1 })
+  }
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => { return prev === 1 ? 0 : prev + 1 })
+  }
 
   return (
-    <div className="explore-section">
-      <div className="section-text">
-        Explore games and experiences on Meta Quest
+    <div className="slider">
+      <div className="container" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
       </div>
-      <button className="browse-button">Browse apps and games</button>
 
-      <div className="carousel-container">
-        <Slider {...carouselSettings}>
-          {images.map((image, index) => (
-            <div key={index}>
-              <img src={image.src} alt={image.alt} />
-            </div>
-          ))}
-        </Slider>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <WestOutlinedIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <EastOutlinedIcon />
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ExploreSection;
+export default ExploreSection
+
+
+
+
+
+
+
